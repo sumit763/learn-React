2f8297145dfa1b7b782acee0bbee195d3b29444a
sumit <sumityadav08449@gmail.com>
2024-04-13 20:49:28 +0530

    useState hook done

--- 01vite/README.md
+++ 01vite/README.md
@@ -1,4 +1,5 @@
 # React + Vite
+# useState hook is used 
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- 01vite/src/App.jsx
+++ 01vite/src/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState(0);
+  const [name, setName] = useState('');
+  const [statusValue, setStatusValue] = useState('');
+
+  function increaseValue() {
+    if (value <= 20) {
+      setValue(value + 1);
+      setStatusValue('Increase');
+    }
+  }
+
+  function decreaseValue() {
+    if (value !== 0) {
+      setValue(value - 1);
+      setStatusValue('Decrease');
+    }
+  }
+
   return (
     <div>
-      <h1>chai or react with vite</h1>
+      <input type="text" onChange={(e) => setName(e.target.value)} />
+
+      <h1>Counter: {value}</h1>
+      <h1>{name} has clicked {statusValue.toLowerCase()} {value} times !!</h1>
+      <button onClick={increaseValue}>Increase</button>
+      <button onClick={decreaseValue}>Decrease</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+ 
